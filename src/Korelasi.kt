f56d0b92829695ce7f813fad9abb43a4913384a3
wiryadev <abrarwiryawan@gmail.com>
2022-02-26 14:06:21 +0700

    adjust table UI for real task

--- src/Korelasi.kt
+++ src/Korelasi.kt
@@ -7,20 +7,20 @@ fun main() {
 //    val y = arrayOf(70, 75, 80, 80, 85, 85, 85, 90, 95, 100)
 
     // Sample 2
-    val x = arrayOf(1, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9)
-    val y = arrayOf(1, 4, 2, 4, 6, 2, 3, 7, 5, 4, 9, 8)
+//    val x = arrayOf(1, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9)
+//    val y = arrayOf(1, 4, 2, 4, 6, 2, 3, 7, 5, 4, 9, 8)
 
     // Task
-//    val x = listOf(
-//        15, 20, 25, 16, 12, 26, 22, 14, 24, 18,
-//        27, 21, 11, 26, 17, 19, 22, 13, 24, 15,
-//        19, 21, 15, 25, 14, 23, 16, 22, 13, 19
-//    )
-//    val y = listOf(
-//        35, 39, 41, 33, 32, 44, 38, 34, 40, 37,
-//        44, 37, 31, 41, 36, 35, 40, 34, 42, 36,
-//        37, 41, 38, 42, 32, 39, 34, 43, 31, 41
-//    )
+    val x = listOf(
+        15, 20, 25, 16, 12, 26, 22, 14, 24, 18,
+        27, 21, 11, 26, 17, 19, 22, 13, 24, 15,
+        19, 21, 15, 25, 14, 23, 16, 22, 13, 19
+    )
+    val y = listOf(
+        35, 39, 41, 33, 32, 44, 38, 34, 40, 37,
+        44, 37, 31, 41, 36, 35, 40, 34, 42, 36,
+        37, 41, 38, 42, 32, 39, 34, 43, 31, 41
+    )
 
     val xPow = x.map { it * it }
     val yPow = y.map { it * it }
@@ -35,9 +35,10 @@ fun main() {
         val xy = x[i] * y[i]
         totalXy += xy
 
-        println("\t\t${x[i]}\t\t${y[i]}\t\t$xy\t${xPow[i]}\t${yPow[i]}")
+        val additionalTabX2 = if (xy < 1000) "\t" else ""
+        println("\t\t${x[i]}\t\t${y[i]}\t\t$xy\t$additionalTabX2${xPow[i]}\t\t${yPow[i]}")
     }
-    println("Total:\t${x.sum()}\t${y.sum()}\t\t$totalXy\t${xPow.sum()}\t${yPow.sum()}")
+    println("Total:\t${x.sum()}\t\t${y.sum()}\t$totalXy\t${xPow.sum()}\t${yPow.sum()}")
     println()
 
     val n = x.size
